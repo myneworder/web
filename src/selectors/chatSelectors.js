23e86a482524519098f554f8fe04d53b3e47e402
Renée Kooi <renee@kooi.me>
2017-02-24 12:45:58 +0100

    Use new roles system throughout.
    Sorting users display by the amount of roles they have.

--- src/selectors/chatSelectors.js
+++ src/selectors/chatSelectors.js
@@ -11,7 +11,8 @@ import {
 import {
   usersSelector,
   currentUserSelector,
-  currentUserHasRoleSelector
+  currentUserHasRoleSelector,
+  createRoleCheckSelector
 } from './userSelectors';
 import {
   notificationSettingsSelector
@@ -80,4 +81,4 @@ export const emojiCompletionsSelector = createSelector(
   }))
 );
 
-export { isModeratorSelector as canDeleteMessagesSelector } from './userSelectors';
+export const canDeleteMessagesSelector = createRoleCheckSelector('chat.delete');
